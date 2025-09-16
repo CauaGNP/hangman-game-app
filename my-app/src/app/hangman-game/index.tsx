ca0b49bbfdc9fa4afba330f2c2c16bfd3e06baf7
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-09-16 23:34:57 +0000

    Add compare function and function for display values

--- my-app/src/app/hangman-game/index.tsx
+++ my-app/src/app/hangman-game/index.tsx
@@ -4,8 +4,10 @@ import { useState } from 'react';
 import Input from '../../components/Input';
 
 export default function HangmanGame() {
-  const [attempts, setAtempts] = useState(0);
-  const [userWord, setUserWord] = useState<string>();
+  const [corretLetter, setCorretLetter] = useState<string[]>([]); 
+  const [prevLetters, setPrevLetters] = useState<string[]>([]); 
+  const [attempts, setAtempts] = useState<number>(0);
+  const [userLetter, setUserLetter] = useState<string>(); 
   const [wordSplit, setWorldSplit] = useState<string[]>([]);
 
   const words = [
@@ -16,11 +18,8 @@ export default function HangmanGame() {
   function randomWord(){
     const selectWord = words[Math.floor((Math.random() * words.length))];
     const splitWord = selectWord.split("");
+    insertValues(selectWord.length);
     return setWorldSplit(splitWord);
-  }
-
-  function compareWords(){
-    
   }
 
   function displayWord(){
@@ -29,16 +28,37 @@ export default function HangmanGame() {
     setWorldSplit([]);
   }
 
+  function insertValues(num : number){
+    setCorretLetter(prev => {
+      const newLetters = [...prev];
+      for(let i = 0; i < num; i++){
+        newLetters[i] = "-"; 
+      }
+      return newLetters;
+    })
+  }
+
+  function compareWords(){
+    const newLetters = [...corretLetter];
+    for(let i = 0; i < wordSplit.length; i++ ){
+        if(wordSplit[i] === userLetter){
+          newLetters[i] = userLetter;
+      }}
+      setCorretLetter(newLetters);
+  }
+
   return (
     <View style={styles.container}>
         { wordSplit.length === 0 ? 
         <Button onPress={() => randomWord()} title="Começar"/> :
         <View>
           <View>
-            {/* {Vai mostrar a palavra} */}
+            {wordSplit.map((_, index) => (
+              <Text key={index}>{ corretLetter[index] === "-" ? "_" : corretLetter[index] }</Text>
+            ))}
           </View>
           <View>
-            <Input onChangeText={(e) => setUserWord(e)} maxLength={1}/>
+            <Input onChangeText={(e) => {setUserLetter(e.toLowerCase())}} maxLength={1}/>
             <View>
               <Button onPress={() => compareWords()} title="Enviar"/>  
               <Button onPress={() => displayWord()} title="Mostrar"/>
